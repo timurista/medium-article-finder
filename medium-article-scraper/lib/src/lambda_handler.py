a3d2442ef6ec3b0f4fab61786e77fcf6c8d25720
Tim Urista <timothy.urista@gmail.com>
2019-12-23 08:18:00 -0800

    update lambda crawler

--- medium-article-scraper/lib/src/lambda_handler.py
+++ medium-article-scraper/lib/src/lambda_handler.py
@@ -2,7 +2,11 @@ import boto3
 import scrapy
 import feedparser
 from scrapy.crawler import CrawlerProcess
+from scrapy.selector import Selector
 import os
+import pprint
+import re
+pp = pprint.PrettyPrinter(depth=6)
 
 s3 = boto3.client('s3')
 BUCKET = os.environ.get('BUCKET', 'sample-bucket')
@@ -12,61 +16,87 @@ class BlogSpider(scrapy.Spider):
     urls = [
         "https://medium.com/feed/tag/ai",
         "https://medium.com/feed/tag/cloud-computing",
+        "https://medium.com/feed/tag/deep-learning",
         "https://medium.com/feed/@timothy.urista"
     ]
     links = []
     items = []
     for url in urls:
        feed = feedparser.parse(url)
-       print(feed['items'][0])
        feed_items = feed['items']
-       links.append([ x['link'] for x in feed_items])
-       items.append(feed_items)
-    
+       for x in feed_items:
+           links.append(x['link'])
+           items.append(x)
 
-    start_urls = [*links]
 
-    print('START', start_urls)
+    start_urls = links
+    # print('START', start_urls)
+
     def parse(self, response):
-        for link in response.css(".list-identifier a[title=\"Abstract\"]::attr('href')").extract():
-            yield response.follow(link, callback=self.parse_detail)
+        for article in response.css("article").extract():
+            # print("LINK", response.url)
+            return self.parse_detail(article, response.url)
+            # yield response.follow(link, callback=self.parse_detail)
 
-    def parse_detail(self, item):
-        host = item.url.split("/abs")[0]
-        pdf_link = item.css('.extra-services li > a::attr(href)').extract_first()
-        abstract = item.css('.abstract').extract()
-        abstract_text = item.css('.abstract::text').extract()
-        title = item.css('h1.title::text').get()
-        authors = item.css('.authors > a::text').extract()
-        submission_date = ""
+    def parse_detail(self, article, link):
+        # print("LINK DETAIL ARTICLE")
+        # pdf_link = item.css('.extra-services li > a::attr(href)').extract_first()
+        # abstract = item.css('.abstract').extract()
+        # abstract_text = item.css('.abstract::text').extract()
+        # title = item.css('h1.title::text').get()
+        # authors = item.css('.authors > a::text').extract()
+        # submission_date = ""
+        # try:
+        #     submission_date = item.css('.submission-history::text').extract()[-1].split(' (')[0]
+        #     submission_date = submission_date.strip()
+        # except Exception as e:
+        #     print(e)
+        
+        blog_post = next(y for y in self.items if y['link'] == link)
+        tags = []
+        image = ''
+        dscription = ''
         try:
-            submission_date = item.css('.submission-history::text').extract()[-1].split(' (')[0]
-            submission_date = submission_date.strip()
+            if (blog_post['tags']):
+                tags = [x['term'] for x in blog_post['tags']]
+        except KeyError:
+            print('no tags')
+        
+        try:
+            matches = re.search('src="([^"]+)"', blog_post['summary'])
+            image = matches[1]
+
+            stat_match = re.search('stat[?]event', image)
+            if stat_match:
+                image = ""
         except Exception as e:
             print(e)
 
+        # snippet = Selector(blog_post.description).css('.medium-feed-snippet').extract()
         yield {
-            "title": title,
-            "submission_date": submission_date,
-            "authors": authors,
-            "pdf_link": f"{host}{pdf_link}.pdf",
-            "abstract": abstract[0],
-            "abstract_text": abstract_text[0]
+            "link": link,
+            "title": blog_post.title,
+            "content": article,
+            "description": blog_post.description,
+            "author": blog_post.author,
+            "published": blog_post.published,
+            "tags": tags,
+            "image": image
         }
 
 def main(event, context):
     process = CrawlerProcess({
         'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
         'FEED_FORMAT': 'json',
-        'FEED_URI': '/tmp/result.json'
+        'FEED_URI': '/tmp/blog_result.json'
     })
 
     process.crawl(BlogSpider)
     process.start() # the script will block here until the crawling is finished
 
-    data = open('/tmp/result.json', 'rb')
+    data = open('/tmp/blog_result.json', 'rb')
 
-    s3.put_object(Bucket = BUCKET, Key='blogs/papers.json', Body=data)
+    # s3.put_object(Bucket = BUCKET, Key='blogs/papers.json', Body=data)
     print('All done.')
 
 if __name__ == "__main__":
